3cdc7e922360d7d29fefb4efbc8461ce07ba0731
jasonmiles <writejasonmiles@icloud.com>
2016-12-07 15:38:47 +0800

    Added file upload functionality using multer

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express')
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/'})
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello World')
+  res.sendfile('index.html')
+})
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  res.send({size: req.file.size.toString()});
 })
 
 app.listen('8080', function() {
